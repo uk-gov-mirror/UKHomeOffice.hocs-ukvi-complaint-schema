1a2386583631f170da34ddd3e56c072efff49736
Aaron Robinson <aaron.robinson@digital.homeoffice.gov.uk>
2021-04-07 11:24:14 +0100

    Add max size tests for each complaint type

--- src/test/java/uk/gov/digital/ho/hocs/correspondence/JSONValidate.java
+++ src/test/java/uk/gov/digital/ho/hocs/correspondence/JSONValidate.java
@@ -2,7 +2,6 @@ package uk.gov.digital.ho.hocs.correspondence;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.networknt.schema.*;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -151,12 +150,123 @@ public class JSONValidate {
         }
     }
 
+    @Test
+    public void existingMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/existing.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void existingOtherMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/existingOther.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void makingAppointmentMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/makingAppointment.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void poorInformationMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/poorInformation.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void biometricMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/biometric.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void staffBehaviourMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/staffBehaviour.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void submittingApplicationMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/submittingApplication.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void somethingElseComplaintMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/somethingElse.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void delaysComplaintMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/delays.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void decisionComplaintMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/decision.json")
+        ) {
+            testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
+    @Test
+    public void refundComplaintMaxSize() throws Exception {
+        try (
+                InputStream schemaStream = inputStreamFromClasspath("cmsSchema.json");
+                InputStream jsonStream = inputStreamFromClasspath("jsonComplaintExamples/maxSize/refund.json")
+        ) {
+                   testSchemaValid(schemaStream, jsonStream);
+        }
+    }
+
     private void testSchemaValid(InputStream schemaStream, InputStream jsonStream) throws IOException {
-        JsonNode json = objectMapper.readTree(jsonStream);
+        byte[] jsonBytes = jsonStream.readAllBytes();
+        JsonNode json = objectMapper.readTree(jsonBytes);
         JsonSchema schema = schemaFactory.getSchema(schemaStream);
         Set<ValidationMessage> validationResult = schema.validate(json);
         if (validationResult.isEmpty()) {
-            assertTrue(true);
+            assertTrue(jsonBytes.length < 250000);
         } else {
             for (ValidationMessage validationMessage : validationResult) {
                 log.info(validationMessage.getMessage());
